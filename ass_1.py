531afe9c884daa94b0d7abddb33b34ab694791a2
Jeyasri J <153058622+Jeyasri2002@users.noreply.github.com>
2023-12-06 12:45:56 +0530

    Update ass_1.py

--- ass_1.py
+++ ass_1.py
@@ -4,6 +4,10 @@ Created on Tue Dec  5 19:29:31 2023
 
 @author: Dell
 """
+"""Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal 
+substring consisting of non-space characters only.
+"""
 
 def len_of_last_word(s):
     word=s.split()
